bba823298f3a672c6761a182a691a2f3ae99d784
yuhyerin <hyerin77@naver.com>
2022-03-31 00:43:56 +0900

    project 생성 / 초기 세팅

---
+++ hyerinToyProject/src/main/java/com/riri/HyerinToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.riri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyerinToyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyerinToyProjectApplication.class, args);
+	}
+
+}
